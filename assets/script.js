4207a27bd186a4522c17d618afa96fbccfc850c6
LukasBartee <lukasbartee@gmail.com>
2022-09-14 22:40:40 -0700

    prototyped stock data api call

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,11 @@ const userAction = async () => {
     console.log(myJson);
 }
 
+const userAction2 = async () => {
+    const response = await fetch('https://api.polygon.io/v2/aggs/ticker/SPY/range/1/week/2022-09-07/2022-09-14?adjusted=true&sort=desc&limit=120&apiKey=I9u6_SnBzPfzZMdaFDN4jRPfwr6wYPsJ');
+    const myJson = await response.json();
+    console.log(myJson);
+}
+
 userAction();
+userAction2();
