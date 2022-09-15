c23a6bed4a2831d4f0cb581f9c0423e1212a4c2b
LukasBartee <lukasbartee@gmail.com>
2022-09-15 13:08:09 -0700

    further api prototype

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,30 @@
-const userAction = async () => {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&past_days=7');
-    const myJson = await response.json();
-    console.log(myJson);
+const pullWeather = async () => {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&past_days=5');
+    const weatherJson = await response.json();
+ //   console.log(weatherJson);
+    return weatherJson;
 }
 
-const userAction2 = async () => {
-    const response = await fetch('https://api.polygon.io/v2/aggs/ticker/SPY/range/1/week/2022-09-07/2022-09-14?adjusted=true&sort=desc&limit=120&apiKey=I9u6_SnBzPfzZMdaFDN4jRPfwr6wYPsJ');
-    const myJson = await response.json();
-    console.log(myJson);
+const pullMarket = async () => {
+    const response = await fetch('https://api.polygon.io/v2/aggs/ticker/SPY/range/1/day/2022-09-08/2022-09-15?adjusted=true&sort=desc&limit=5000&apiKey=I9u6_SnBzPfzZMdaFDN4jRPfwr6wYPsJ');
+    const marketJson = await response.json();
+  //  console.log(marketJson);
+    return marketJson;
 }
 
-userAction();
-userAction2();
+
+const getMarket = async () => {
+    var rawMarket = pullMarket();
+    //console.log(rawMarket);
+    console.log(rawMarket.);
+
+}
+
+const getWeather = async () => {
+    var rawWeather = pullWeather();
+    //console.log(rawWeather);
+
+}
+
+getMarket();
+getWeather();
