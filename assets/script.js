e6444bbe708cc25dd9bc1cdd4f39ea99d346da84
LukasBartee <lukasbartee@gmail.com>
2022-09-18 15:23:19 -0700

    tailwind handoff update

--- assets/script.js
+++ assets/script.js
@@ -7,58 +7,48 @@ var today = new Date().toJSON().slice(0, 10);
 var past = new Date();
 past.setDate(past.getDate() - daySpan - 1);
 var pastDate = past.getFullYear() + '-' + (past.getMonth() + 1).toString().padStart(2, "0") + '-' + past.getDate().toString().padStart(2, "0");
-console.log(pastDate);
-console.log(today);
 
 const pullWeather = async () => {
     //pull weather JSON from open meteo
     const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&past_days=' + daySpan);
     const weatherJson = await response.json();
-    console.log(weatherJson);
 
-    
+    //format and return daily high temperature as linear array
     var tempList = [];
     for (i = 0; i <= daySpan; i++) {
         tempList[i] = weatherJson.daily.temperature_2m_max[i];
     }
-    console.log(tempList);
-    //console.log(weatherJson.daily.temperature_2m_max[0]);
-    return weatherJson;
+
+    return tempList;
 }
 
-console.log('https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/day/2022-09-08/' + today + '?adjusted=true&sort=desc&limit=5000&apiKey=' + APIKey)
-
 const pullMarket = async () => {
-    //const response = await fetch('https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/day/2022-09-08/2022-09-16?adjusted=true&sort=desc&limit=5000&apiKey=' + APIKey);
-
+    //pull closing price from ploygon.io
     const response = await fetch('https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/day/' + pastDate + '/' + today + '?adjusted=true&sort=desc&limit=5000&apiKey=' + APIKey);
-
     const marketJson = await response.json();
 
+    //format and return daily closing price as linear array
     var closeList = [];
-    for (i = 0; i <= daySpan -1; i++) {
+    for (i = 0; i < daySpan; i++) {
         closeList[i] = marketJson.results[i].c;
     }
 
-    console.log(marketJson);
-    console.log(closeList);
-    return marketJson;
+    return closeList;
 }
 
 
-const getMarket = async () => {
-    var rawMarket = pullMarket();
-    //console.log(rawMarket);
-    //var data = JSON.parse(rawMarket);
-    //console.log(rawMarket.results);
+const getDeviation = async (dataSet) => {
+    //calculate and return percentage change across dataset as linear array;
+    for (i = 0; i < dataSet.length; i++) {
+        console.log(i);
+        console.log("touch");
+
+    }
 
 }
-
-const getWeather = async () => {
-    var rawWeather = pullWeather();
-    //console.log(rawWeather);
-
-}
-
-getMarket();
-getWeather();
+var temp = pullWeather();
+var close = pullMarket();
+getDeviation(temp);
+getDeviation(close);
+//getMarket();
+//getWeather();
